f29689effb31e5a84eacb2acaa189a2e43dac1a2
Hanna Dziahonskaya <svetlova.hanna@mail.ru>
2023-03-13 21:27:35 +0300

    add zod validation

--- product-service/src/createProduct.ts
+++ product-service/src/createProduct.ts
@@ -3,22 +3,23 @@ import {
   APIGatewayProxyResultV2,
 } from 'aws-lambda';
 import { ProductService } from 'services/types';
+import { validateProductData } from 'utils/validation';
 
 export const createProduct = (productService: ProductService) => async (event: APIGatewayProxyEventV2, _context): Promise<APIGatewayProxyResultV2> => {
   try {
-    // const dbService = new DynamoDbProductService('products-table');
     console.log('Create event', event);
     const productData = JSON.parse(Buffer.from(event.body, 'base64').toString());
     console.log('Product data', productData);
+    validateProductData(productData);
     const product = await productService.createProduct(productData);
     console.log('Products list', product);
 
     return {
       statusCode: 201,
-      // headers: {
-      //   'Access-Control-Allow-Origin': '*',
-      //   'Access-Control-Allow-Credentials': true
-      // },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
       body: JSON.stringify(product),
     };
   } catch (error) {
